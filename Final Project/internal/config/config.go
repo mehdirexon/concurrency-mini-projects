52d3a49b62f63ab1a45b83671912ca424e251004
Mehdi Ghazanfari <m.ghazanfari1384@gmail.com>
2025-09-30 22:37:28 +0330

    Application configuration: Add config package with session and Redis setup

---
+++ Final Project/internal/config/config.go
@@ -0,0 +1,17 @@
+package appconfig
+
+import (
+	"log"
+	"sync"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+type AppConfig struct {
+	InfoLogger   *log.Logger
+	ErrorLogger  *log.Logger
+	Session      *scs.SessionManager
+	Wait         *sync.WaitGroup
+	UseCache     bool
+	InProduction bool
+}
---
+++ Final Project/internal/config/redis.go
@@ -0,0 +1,16 @@
+package appconfig
+
+import (
+	"os"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+func RedisInit() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+}
