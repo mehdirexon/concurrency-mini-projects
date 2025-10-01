42497fe7fb3f5284e9842c9a39c57dcecdcc20c4
Mehdi Ghazanfari <m.ghazanfari1384@gmail.com>
2025-10-01 05:21:18 +0330

    Update store interface to work with new database models and add plan functions

--- Final Project/internal/store/Store.go
+++ Final Project/internal/store/Store.go
@@ -1,4 +1,22 @@
 package store
 
+import "final-project/internal/database"
+
 type Store interface {
+	// GetAllUsers Users
+	GetAllUsers() ([]*database.User, error)
+	GetUserByID(id int) (*database.User, error)
+	GetUserByEmail(email string) (*database.User, error)
+	InsertUser(user database.User) (int, error)
+	UpdateUser(u *database.User) error
+	DeleteUser(u *database.User) error
+	DeleteUserByID(id int) error
+	ResetUserPassword(u *database.User, password string) error
+	UserPasswordMatches(u *database.User, plainText string) (bool, error)
+
+	// GetAllPlans Plans
+	GetAllPlans() ([]*database.Plan, error)
+	GetPlanByID(id int) (*database.Plan, error)
+	SubscribeUserToPlan(user database.User, plan database.Plan) error
+	AmountForDisplay(plan database.Plan) string
 }
